7c2316c071c85d64134e4ac231aa5a0a665d0ad8
Hao Duong <hao.duong11@gmail.com>
2023-11-17 16:19:06 +0900

    Add file uploading functionality

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,33 @@
+import { db } from '@/db';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { createUploadthing, type FileRouter } from 'uploadthing/next';
+
+const f = createUploadthing();
+
+const auth = async () => {};
+
+export const ourFileRouter = {
+  pdfUploader: f({ pdf: { maxFileSize: '4MB' } })
+    .middleware(async ({ req }) => {
+      const { getUser } = getKindeServerSession();
+      const user = await getUser();
+
+      if (!user || !user.id) throw new Error('UNAUTHORIZED');
+
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      const createdFile = await db.file.create({
+        data: {
+          key: file.key,
+          name: file.name,
+          userId: metadata.userId,
+          url: file.url,
+          uploadStatus: 'PROCESSING',
+        },
+      });
+      return {};
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
