ad6e3c1240e83b98d73dd6bcb2da3538eebad8da
Hao Duong <hao.duong11@gmail.com>
2023-11-28 20:20:14 +0900

    Integrate subscription plan with stripe

---
+++ src/config/stripe/stripe.ts
@@ -0,0 +1,28 @@
+export const PLANS = [
+  {
+    name: 'Free',
+    slug: 'free',
+    quota: 10,
+    pagesPerPDF: 5,
+    price: {
+      amount: 0,
+      priceIds: {
+        test: '',
+        production: '',
+      },
+    },
+  },
+  {
+    name: 'Pro',
+    slug: 'pro',
+    quota: 50,
+    pagesPerPDF: 25,
+    price: {
+      amount: 14.99,
+      priceIds: {
+        test: 'price_1OHOL9IYnQEC65U9lb7D80ua',
+        production: '',
+      },
+    },
+  },
+];
